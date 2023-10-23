a9ca84bd0ada2fa125651e454678c4343a20052e
Ranvir Sahota <ranvirsahota.dev@gmail.com>
2023-10-23 15:11:09 +0100

    code tested and complete

---
+++ hangman/milestone_5.py
@@ -0,0 +1,50 @@
+import random
+class Hangman:
+    def __init__(self, word_list, num_lives = 5):
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for char in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_gusses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f'Good guess! {guess} is in the word')
+            for index in range(len(self.word)):
+                if guess == self.word[index]: self.word_guessed[index] = guess
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f'Sorry, {guess} is not in the word. Try again.')
+            print(f'You have {self.num_lives} lives left.')
+
+    def ask_for_input(self):
+        print(f'Guessed so far: {self.word_guessed}')
+        guess = input('Guess a letter: ')
+        if not guess.isalpha() or len(guess) != 1:
+            print('Invalid letter. Please, enter a single alphabetical character')
+        elif guess in self.list_of_gusses:
+            print('You already tried that letter!')
+        else:
+            self.check_guess(guess)
+            self.list_of_gusses.append(guess)
+
+def play_game(word_list):
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+
+    while True:
+        if game.num_lives == 0:
+            print('You lost')
+            print('The word was:', game.word)
+            return
+        elif game.num_letters > 0:
+            game.ask_for_input()
+        else:
+            print('Congratulations. You won the game!')
+            return
+
+
+play_game(['apples',  'banana', 'blueberriers', 'melons', 'corn'])
